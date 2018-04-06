784a1204c027aa0febb6ec8999fa7f7682f62cda
Liz McKenzie <elizabethbraggisgreat@gmail.com>
2018-04-06 14:03:25 -0400

    setting up routers still

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -1,4 +1,4 @@
 const knex = require('knex');
 const knexConfig = require('../knexfile.js');
-
+//this is okay blank for now, don't freak out
 module.exports = knex(knexConfig.development);
--- server.js
+++ server.js
@@ -1,9 +1,33 @@
 const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
+
+// const actionsRouter = require('./routers/actionsRouter.js');
+// const projectsRouter = require('./routers/projectsRouter.js');
+
 const server = express();
-const port = 5000;
+
+// Custom Middleware
+
+// const logger = (req, res, next) => {
+//   console.log(`Requesting: ${req.url}`);
+//   console.log(`Body being passed: ${req.body}`);
+
+//   next();
+// };
+// server.use(logger);
 
 server.use(express.json());
+server.use(morgan('dev'));
+server.use(helmet());
+server.use(cors());
+
+// server.use('/api/actions', actionsRouter);
+// server.use('/api/projects', projectsRouter);
+
+const port = 5000;
 
 server.listen(port, () => {
-  console.log('NO SLEEP.. TILL SPRINTS OVER');
+  console.log('NO.. SLEEP.. TILL.. SPRINTS OVER');
 });
